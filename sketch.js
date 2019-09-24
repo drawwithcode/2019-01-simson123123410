bd502aecc26e11349959b459aa5e4f4ef07c0a21
simsonchen <simson123123410@gmail.com>
2019-09-25 00:37:33 +0200

    assignment2

--- sketch.js
+++ sketch.js
@@ -1,11 +1,30 @@
-function preload(){
-  // put preload code here
-}
-
 function setup() {
-  // put setup code here
+ createCanvas(750, 750);
+  angleMode(DEGREES);
+  background (60);
+  noFill();
+  frameRate(120);
+
 }
 
-function draw() {
-  // put drawing code here
+function  draw() {
+  translate(width/2,height/2);
+  rotate(frameCount*83);
+  ellipse(100,0,50,400);
+ stroke(
+    lerpColor(
+      color("#8FFFFA"),
+      color("#FF6D60"),
+
+      frameCount / 200
+    ));
+
+  ellipse(100,0,250);
+  if(frameCount == 350)noLoop();
+
+    ellipse(230,30,50);
+  if(frameCount == 20);
+  ellipse(300,50,20);
+
 }
+    
